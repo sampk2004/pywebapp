6c9e1054b3684bf59850b2fe23c4719bf472fa18
sampk2004 <sam.park1993@gmail.com>
2018-11-12 00:53:44 -0800

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-$(aws ecr get-login --no-include-email --region us-west-2)
-docker pull 012128242757.dkr.ecr.us-west-2.amazonaws.com/sampk2004/pywebapp:latest || {
+$(aws ecr get-login --no-include-email --region us-east-1)
+docker pull 012128242757.dkr.ecr.us-east-1.amazonaws.com/sampk2004/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
 }
-docker run --name pywebapp -p 80:8080 --detach 012128242757.dkr.ecr.us-west-2.amazonaws.com/sampk2004/pywebapp:latest
+docker run --name pywebapp -p 80:8080 --detach 012128242757.dkr.ecr.us-east-1.amazonaws.com/sampk2004/pywebapp:latest
